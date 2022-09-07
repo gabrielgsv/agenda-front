02b65ff7408a253703f7eab3b5fbe7e6896965cb
gabrielgsv <gabriel.soares.vilela@gmail.com>
2022-09-07 16:37:35 -0300

    add login screen and connect with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { MantineProvider } from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes";
 
 function App() {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        primaryColor: "teal",
+        defaultRadius: "lg",
+      }}
+    >
+      <NotificationsProvider>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </NotificationsProvider>
     </MantineProvider>
   );
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,17 @@
-import { Center } from "@mantine/core";
+import { Card, Center, Image } from "@mantine/core";
+import image from "../../assets/login.svg";
+import LoginForm from "./LoginForm";
 
 const Login = () => {
   return (
     <>
-      <Center style={{ height: "100vh" }}>
-        <h1>Login</h1>
+      <Center sx={{ height: "100vh" }}>
+        <Card shadow="sm" p="lg" radius="md" withBorder>
+          <Card.Section sx={{ padding: 20 }}>
+            <Image src={image} width={300} alt="Login draw" />
+          </Card.Section>
+          <LoginForm />
+        </Card>
       </Center>
     </>
   );
