446cc365d404ba67290b47882321d054374d9d12
gabrielgsv <gabriel.soares.vilela@gmail.com>
2022-10-17 16:22:40 -0300

    add daysNotification to Redux

--- src/redux/calendar/calendarSlicer.ts
+++ src/redux/calendar/calendarSlicer.ts
@@ -1,14 +1,27 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 // Define a type for the slice state
+interface IDaysNotification {
+  [x: number]: {
+    [x: number]: number[];
+  };
+}
 interface CalendarState {
   value: Date;
+  daysNotification: IDaysNotification | any;
 }
 
 // Define the initial state using that type
 const initialState: CalendarState = {
   value: new Date(),
+  daysNotification: null,
 };
+
+interface IDaysParams {
+  year: number;
+  month: number;
+  days: number[];
+}
 
 export const calendarSlice = createSlice({
   name: "calendar",
@@ -18,9 +31,36 @@ export const calendarSlice = createSlice({
     handleCalendar: (state, action: PayloadAction<Date>) => {
       state.value = action.payload;
     },
+    handleDaysNotification: (state, action: PayloadAction<IDaysParams>) => {
+      const params = action.payload;
+      state.daysNotification = {
+        ...state.daysNotification,
+        [params.year]: {
+          ...state.daysNotification?.[params.year],
+          [params.month]: params.days,
+        },
+      };
+    },
+    handleAddDayNotificatiion: (state, action: PayloadAction<IDaysParams>) => {
+      const params = action.payload;
+      state.daysNotification = {
+        ...state.daysNotification,
+        [params.year]: {
+          ...state.daysNotification?.[params.year],
+          [params.month]: [
+            ...state.daysNotification?.[params.year]?.[params.month],
+            ...params.days,
+          ],
+        },
+      };
+    },
   },
 });
 
-export const { handleCalendar } = calendarSlice.actions;
+export const {
+  handleCalendar,
+  handleDaysNotification,
+  handleAddDayNotificatiion,
+} = calendarSlice.actions;
 
 export default calendarSlice.reducer;
